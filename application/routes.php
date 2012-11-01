b3029ca45d0a84954ac32a390915dddbfac9116b
Corey McMahon <corey.mcmahon@bigcommerce.com>
2012-11-01 11:32:28 +1100

    Initial commit

--- application/routes.php
+++ application/routes.php
@@ -37,6 +37,8 @@ Route::get('/', function()
 	return View::make('home.index');
 });
 
+Route::controller('Experiments');
+
 /*
 |--------------------------------------------------------------------------
 | Application 404 & 500 Error Handlers
